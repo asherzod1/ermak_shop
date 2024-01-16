cb1808072d83a1ff49b12233fbfd565db568b7f8
Abdumutalov <sh.abdumutalov1@gmail.com>
2024-01-16 18:03:50 +0500

    First commit 3

--- ermak_shop/settings.py
+++ ermak_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6rlv3kb_=dkyvvgznf^@%z1w13$r40^t=!z)ishhzeut$9)-cs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
