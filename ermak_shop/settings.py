6710848ac6c7c6330fb5f620551eaac19a8a36a2
Abdumutalov <sh.abdumutalov1@gmail.com>
2024-01-18 17:17:50 +0500

    Models

--- ermak_shop/settings.py
+++ ermak_shop/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ermak',
         'USER': 'postgres',
-        'PASSWORD': 'Lion5466.',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
